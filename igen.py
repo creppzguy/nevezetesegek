4345013fa97afcd9ea8159ec9ed96e18b90735f7
The Last Tarnished in the Lands Between <74605307+creppzguy@users.noreply.github.com>
2022-03-23 09:30:27 +0100

    Add files via upload

--- igen.py
+++ igen.py
@@ -1,5 +1,13 @@
 f = open("g.txt", "r") 
-print(f.read())
+#print(f.read())
+nagylista=[]
 for sor in f:
-    idő = input()
-    print("Mikor jött létre?: " + idő)
+    kislista=sor[:-1].split(";")
+    nagylista.append(kislista)
+f.close()
+#Menü rendszer
+print(1.Menü pont)
+print(2.menü pont)
+print(3.Menü pont)
+print(4.Menü pont)
+print(4.Menü pont)
