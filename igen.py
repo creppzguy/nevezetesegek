0750bc66ecc01f84f2a1798ed1ebd71083156bbb
The Last Tarnished in the Lands Between <74605307+creppzguy@users.noreply.github.com>
2022-03-24 14:29:25 +0100

    Add files via upload

--- igen.py
+++ igen.py
@@ -6,8 +6,15 @@ for sor in f:
     nagylista.append(kislista)
 f.close()
 #Menü rendszer
-print(1.Menü pont)
-print(2.menü pont)
-print(3.Menü pont)
-print(4.Menü pont)
-print(4.Menü pont)
+print("1= Országnév")
+print("2= Építmény keletkezése")
+print("3 3.Menü pont")
+v=input("Add meg a válaszott menü pont jelét ")
+if v == "1":
+    o=input("Adj meg egy országnevet ")
+    if o == "Horvátország":
+        print("Plitvicei Tavak Nemzeti Park")
+elif v == "2":
+    print("Igen")
+elif v == "3":
+    print("Talán")
