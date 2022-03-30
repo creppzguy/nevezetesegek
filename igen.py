95e632f90f8027ab9b23da754fc2fe023d2a30a5
The Last Tarnished in the Lands Between <74605307+creppzguy@users.noreply.github.com>
2022-03-30 09:28:46 +0200

    Add files via upload

--- igen.py
+++ igen.py
@@ -5,16 +5,41 @@ for sor in f:
     kislista=sor[:-1].split(";")
     nagylista.append(kislista)
 f.close()
+
 #Menü rendszer
 print("1= Országnév")
 print("2= Építmény keletkezése")
-print("3 3.Menü pont")
+print("3.Menü pont")
 v=input("Add meg a válaszott menü pont jelét ")
 if v == "1":
-    o=input("Adj meg egy országnevet ")
+    o=input("Add meg az az országnevét ")
     if o == "Horvátország":
         print("Plitvicei Tavak Nemzeti Park")
+    elif o == "Görögország":
+        print("Santorini, természetes medence")
+    elif o == "Kambodzsa":
+        print("Agkor Thom, Baphuon templom")
+    elif o == "Japán":
+        print("1: Nishinomaru Kert")
+        print("2: Arany Pavilon")
+        print("3: Himeji Kastély")
+        print("4: Dōgo onsen ")
+        print("5: Tokió Torony")
+        j=input("Válasz a nevezetességek közül ")
+        if j == "1":
+            print("Nem")
+        elif j == "2":
+            print("Lehetetlen")
+        elif j == "3":
+            print("Dehogy")
+        elif j == "4":
+            print("Semmiképp")
+        elif j == "5":
+            print("Semmiféleképpen")
 elif v == "2":
-    print("Igen")
+    e=input("Add meg mikor készült az építmény: ")
+    if e == "1949":
+        print ("Plitvicei Tavak Nemzeti Park")
+    
 elif v == "3":
     print("Talán")
